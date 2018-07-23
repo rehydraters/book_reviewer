9a94a5ab91c7a2b2e4e63d58804d78cf96e227af
rehydraters <rehydraters@gmail.com>
2018-07-23 16:10:29 -0500

    on_delete parameter fix

--- apps/brbooks/models.py
+++ apps/brbooks/models.py
@@ -11,15 +11,15 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    author = models.ForeignKey(Author, related_name="books")
+    author = models.ForeignKey(Author, related_name="books", on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
 class Review(models.Model):
     review = models.TextField()
     rating = models.IntegerField()
-    book = models.ForeignKey(Book, related_name="reviews")
-    reviewer = models.ForeignKey(User, related_name="reviews_user")
+    book = models.ForeignKey(Book, related_name="reviews", on_delete=models.PROTECT)
+    reviewer = models.ForeignKey(User, related_name="reviews_user", on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
